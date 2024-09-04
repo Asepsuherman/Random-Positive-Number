3bb907530e68f22a030270250d195b17b585dd1f
Asep <asepsuherman389@gmail.com>
2024-09-04 13:20:16 +0700

    Update Header and Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="p-4 text-center text-white bg-blue-600">
+            <p>Tanggal Pengerjaan: 4 September 2024</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="p-4 bg-blue-600 text-white text-center">
+            <h1>Asep Suherman</h1>
+        </header>
+    );
+};
+
+export default Header;
